c1fde88e246aedf12c3870e2879e7a3819c5c974
albertlast <mathiaspapealbert@hotmail.com>
2020-04-27 16:18:56 +0200

    travis want a licence when the file get moved to the root...
    Signed-off-by: albertlast albertlast@hotmail.de

---
+++ other/db_last_error.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Simple Machines Forum (SMF)
+ *
+ * @package SMF
+ * @author Simple Machines https://www.simplemachines.org
+ * @copyright 2020 Simple Machines and individual contributors
+ * @license https://www.simplemachines.org/about/smf/license.php BSD
+ *
+ * @version 2.1 RC2
+ */
+
+ 
